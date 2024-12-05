e21b60c083f1a08e7c5dc01f39b3eeb54f32ba57
Himanshu Singh <hsnice16@gmail.com>
2024-12-06 03:44:34 +0530

    add: create and place order

--- index.mjs
+++ index.mjs
@@ -1,12 +1,101 @@
 import "dotenv/config";
-import { calculatePrices } from "./utils.mjs";
-import { SPREAD_PERCENTAGE, NUM_LEVELS } from "./constant.mjs";
-
-const { bidPrices, askPrices } = await calculatePrices(
-  "eth",
+import { calculatePrices, createOrder, placeOrder } from "./utils.mjs";
+import {
   SPREAD_PERCENTAGE,
-  NUM_LEVELS
-);
+  NUM_LEVELS,
+  LEVERAGE,
+  LEVEL_LIQUIDITY,
+} from "./constant.mjs";
 
-console.log("bidPrices", bidPrices);
-console.log("askPrices", askPrices);
+await populateEthOrderBook();
+await populateBtcOrderBook();
+
+async function populateEthOrderBook() {
+  const symbol = "ETH";
+
+  const { bidPrices, askPrices } = await calculatePrices(
+    symbol,
+    SPREAD_PERCENTAGE,
+    NUM_LEVELS
+  );
+
+  console.log("eth -- bidPrices", bidPrices);
+  console.log("eth -- askPrices", askPrices);
+
+  console.log("eth -- buy orders");
+
+  // Place orders for buy (Long)
+  for (const price in bidPrices) {
+    const order = createOrder(
+      "L",
+      LEVERAGE,
+      price,
+      LEVEL_LIQUIDITY,
+      symbol,
+      "M"
+    );
+
+    await placeOrder({}, order);
+  }
+
+  console.log("eth -- sell orders");
+
+  // Place orders for sell (Short)
+  for (const price in askPrices) {
+    const order = createOrder(
+      "S",
+      LEVERAGE,
+      price,
+      LEVEL_LIQUIDITY,
+      symbol,
+      "M"
+    );
+
+    await placeOrder({}, order);
+  }
+}
+
+async function populateBtcOrderBook() {
+  const symbol = "BTC";
+
+  const { bidPrices, askPrices } = await calculatePrices(
+    symbol,
+    SPREAD_PERCENTAGE,
+    NUM_LEVELS
+  );
+
+  console.log("btc -- bidPrices", bidPrices);
+  console.log("btc -- askPrices", askPrices);
+
+  console.log("btc -- buy orders");
+
+  // Place orders for buy (Long)
+  for (const price in bidPrices) {
+    const order = createOrder(
+      "L",
+      LEVERAGE,
+      price,
+      LEVEL_LIQUIDITY,
+      symbol,
+      "M"
+    );
+
+    await placeOrder({}, order);
+  }
+
+  console.log("btc -- sell orders");
+
+  // Place orders for sell (Short)
+  for (const price in askPrices) {
+    const order = createOrder(
+      "S",
+      LEVERAGE,
+      price,
+      LEVEL_LIQUIDITY,
+      symbol,
+      "M"
+    );
+
+    await placeOrder({}, order);
+  }
+}
--- utils.mjs
+++ utils.mjs
@@ -1,34 +1,6 @@
 import fetch from "node-fetch";
 
-export { calculatePrices };
-
-async function getOracleReferencePrice(symbol) {
-  const url = process.env.BASE_URL + "/price/" + symbol;
-  const response = await fetch(url);
-  const data = await response.json();
-
-  return data?.data?.latest_price?.mark_price;
-}
-
-function calculateBidPrices(referencePrice, spreadPercentage, levels) {
-  const bidPrices = [];
-
-  for (let index = 0; index < levels; index++) {
-    bidPrices.push(referencePrice * (1 - spreadPercentage * (index + 1)));
-  }
-
-  return bidPrices;
-}
-
-function calculateAskPrices(referencePrice, spreadPercentage, levels) {
-  const askPrices = [];
-
-  for (let index = 0; index < levels; index++) {
-    askPrices.push(referencePrice * (1 + spreadPercentage * (index + 1)));
-  }
-
-  return askPrices;
-}
+export { calculatePrices, placeOrder, createOrder };
 
 async function calculatePrices(symbol, spreadPercentage, levels) {
   const referencePrice = await getOracleReferencePrice(symbol);
@@ -49,4 +21,90 @@ async function calculatePrices(symbol, spreadPercentage, levels) {
     bidPrices,
     askPrices,
   };
+}
+
+/**
+ * 
+ * @param {Object} headers { 
+    X-Platform: string; 
+    X-Chain-EVM-Id: string; 
+    X-Nonce: string; 
+    X-Signature: string 
+  }
+ * 
+ * @param {Object} data { 
+    direction: "L" | "S";
+    expiry_time: number;
+    leverage: number;
+    price: number;
+    quantity: number;
+    slippage: number | undefined;
+    symbol: string;
+    type: "M" | "L";
+  }
+ */
+async function placeOrder(headers, data) {
+  const url = process.env.BASE_URL + "/order/create";
+  const response = await fetch(url, {
+    method: "POST",
+    headers,
+    body: JSON.stringify(data),
+  });
+
+  const jsonData = await response.json();
+  console.log("data", jsonData);
+}
+
+/**
+ *
+ * @param {string} direction "L" | "S"
+ * @param {number} leverage 2
+ * @param {number} price 3222
+ * @param {number} amount 20000
+ * @param {string} symbol "ETH" | "BTC"
+ * @param {string} type "M" | "L"
+ * @returns
+ */
+async function createOrder(direction, leverage, price, amount, symbol, type) {
+  price = parseFloat(price.toFixed(2));
+  const quantity = parseFloat((amount / price).toFixed(2));
+
+  return {
+    direction,
+    expiry_time: 17000000000,
+    leverage,
+    symbol,
+    type,
+    slippage: 2,
+    price,
+    quantity,
+  };
+}
+
+function calculateAskPrices(referencePrice, spreadPercentage, levels) {
+  const askPrices = [];
+
+  for (let index = 0; index < levels; index++) {
+    askPrices.push(referencePrice * (1 + spreadPercentage * (index + 1)));
+  }
+
+  return askPrices;
+}
+
+function calculateBidPrices(referencePrice, spreadPercentage, levels) {
+  const bidPrices = [];
+
+  for (let index = 0; index < levels; index++) {
+    bidPrices.push(referencePrice * (1 - spreadPercentage * (index + 1)));
+  }
+
+  return bidPrices;
+}
+
+async function getOracleReferencePrice(symbol) {
+  const url = process.env.BASE_URL + "/price/" + symbol;
+  const response = await fetch(url);
+  const data = await response.json();
+
+  return data?.data?.latest_price?.mark_price;
 }
